3643d08fc84afe281289762af3800e399ac44ac3
Friso <fw.smit01@gmail.com>
2017-06-19 22:07:49 +0200

    Added cmake

---
+++ CMakeFiles/perceptron.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perceptron.dir/source.cpp.o"
+  "perceptron.pdb"
+  "perceptron"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perceptron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ perceptronConfig.h
@@ -0,0 +1,4 @@
+// the configured options and settings for Tutorial
+#define PERCEPTRON_VERSION_MAJOR 1
+#define PERCEPTRON_VERSION_MINOR 0
+
